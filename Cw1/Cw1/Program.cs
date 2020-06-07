cbcd9d2390b78de51387f0036eb033bce52b3fad
s17567 <66383283+s17567@users.noreply.github.com>
2020-06-07 21:34:34 +0200

    ok
    okl

---
+++ Cw1/Cw1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Cw1
+{
+    class Program
+    {
+
+        public static async Task Main ( string[] args)
+        {
+
+
+
+            
+            var httpClient = new HttpClient();
+
+            var response = await httpClient.GetAsync("http://www.nask.pl");
+           string response_string= await httpClient.GetStringAsync(args[0]);
+            Console.WriteLine(response.StatusCode);
+       //    Console.WriteLine(response_string);
+
+
+            Regex email = new Regex(@"\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*", RegexOptions.IgnoreCase );
+
+
+           MatchCollection wyniki = email.Matches(response_string);
+
+
+            Console.WriteLine(wyniki.Count);
+
+            foreach ( Match match in wyniki)
+            {
+                Console.WriteLine(match.Value.ToString());
+                
+            }
+
+
+        }
+    }
+}
